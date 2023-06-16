c3e9a443891564a6c7df1252f0eaae569941403f
Zygisvaldo <zygiodeze@gmail.com>
2023-06-16 17:09:20 +0300

    fix: apiFeatures exported to a module

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,6 @@
 //const fs = require('fs');
 const Tour = require('./../models/tourModel');
+const APIFeatures = require('./../utils/apiFeatures');
 
 // 5) Aliasing (for example most popular request)
 // midleware will set(prefill) query object
@@ -12,82 +13,89 @@ exports.aliasTopTours = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    console.log(req.query);
+    //console.log(req.query);
 
     // BUILD QUERY
-    // 1A) FILTERING
-    // shallow copy of req.query/ creating new obj
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach(el => delete queryObj[el]);
+    // // 1A) FILTERING
+    // // shallow copy of req.query/ creating new obj
+    // const queryObj = { ...req.query };
+    // const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    // excludedFields.forEach(el => delete queryObj[el]);
 
-    //console.log(req.query, queryObj);
+    // //console.log(req.query, queryObj);
 
-    // const toursData = await Tour.find({
-    //   duration: 5,
-    //   difficulty: 'easy'
-    // });
+    // // const toursData = await Tour.find({
+    // //   duration: 5,
+    // //   difficulty: 'easy'
+    // // });
 
-    // returns query so we can use methods on it
-    // const toursData = await Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy');
+    // // returns query so we can use methods on it
+    // // const toursData = await Tour.find()
+    // //   .where('duration')
+    // //   .equals(5)
+    // //   .where('difficulty')
+    // //   .equals('easy');
 
-    // 1B) Advanced filtering
+    // // 1B) Advanced filtering
 
-    // mongoDB {difficulty: 'easy', duration :{ $gte: 5}}
-    // query obj from url { difficulty: 'easy', duration: { gte: '5' } }
-    // gte, gt, lte, lt
+    // // mongoDB {difficulty: 'easy', duration :{ $gte: 5}}
+    // // query obj from url { difficulty: 'easy', duration: { gte: '5' } }
+    // // gte, gt, lte, lt
 
-    let queryStr = JSON.stringify(queryObj); //
+    // let queryStr = JSON.stringify(queryObj); //
 
-    // replacing regular expressions by adding $ to it.
-    // b - for exact match
-    // g - for multiple occurencies
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-    //console.log(JSON.parse(queryStr));
+    // // replacing regular expressions by adding $ to it.
+    // // b - for exact match
+    // // g - for multiple occurencies
+    // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    // //console.log(JSON.parse(queryStr));
 
-    let query = Tour.find(JSON.parse(queryStr)); // retuns a query so we can chain other methods on it
+    // let query = Tour.find(JSON.parse(queryStr)); // retuns a query so we can chain other methods on it
 
-    // 2) Sorting
-    if (req.query.sort) {
-      // sorting by several parameters
-      const sortBy = req.query.sort.split(',').join(' ');
-      //console.log(sortBy);
-      //query = query.sort(req.query.sort);
-      query = query.sort(sortBy);
+    // // 2) Sorting
+    // if (req.query.sort) {
+    //   // sorting by several parameters
+    //   const sortBy = req.query.sort.split(',').join(' ');
+    //   //console.log(sortBy);
+    //   //query = query.sort(req.query.sort);
+    //   query = query.sort(sortBy);
 
-      // sort('price ratingsAverage')
-    } else {
-      query = query.sort('-createdAt'); // default sorting
-    }
+    //   // sort('price ratingsAverage')
+    // } else {
+    //   query = query.sort('-createdAt'); // default sorting
+    // }
 
-    // 3) Field limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      //query = query.select('name duration price') // seperated by space/ projecting
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v'); // - for excluding fields
-    }
+    // // 3) Field limiting
+    // if (req.query.fields) {
+    //   const fields = req.query.fields.split(',').join(' ');
+    //   //query = query.select('name duration price') // seperated by space/ projecting
+    //   query = query.select(fields);
+    // } else {
+    //   query = query.select('-__v'); // - for excluding fields
+    // }
 
-    // 4) Pagination
-    const page = req.query.page * 1 || 1; // this or || default value
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit; // previous page * limit of results on each page
+    // // 4) Pagination
+    // const page = req.query.page * 1 || 1; // this or || default value
+    // const limit = req.query.limit * 1 || 100;
+    // const skip = (page - 1) * limit; // previous page * limit of results on each page
 
-    // ?page2&limit=10 (1-10 page1), (11-20 page2)
-    query = query.skip(skip).limit(limit); // skip amount of results before quering data (skip 10 to start with 11)
+    // // ?page2&limit=10 (1-10 page1), (11-20 page2)
+    // query = query.skip(skip).limit(limit); // skip amount of results before quering data (skip 10 to start with 11)
 
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments(); // return promise of num of docs
-      if (skip >= numTours) throw new Error('This page does not exists');
-    }
+    // if (req.query.page) {
+    //   const numTours = await Tour.countDocuments(); // return promise of num of docs
+    //   if (skip >= numTours) throw new Error('This page does not exists');
+    // }
 
     // EXECUTE QUERY
-    const toursData = await query;
+    // creating an instance
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate(); // chaining works because we return this
+
+    const toursData = await features.query;
     // query.sort().select().skip().limit() query example
 
     // SEND RESPONSE
