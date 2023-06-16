c114bb28c774601a57b94e33290d78b28dcbf651
Zygisvaldo <zygiodeze@gmail.com>
2023-06-16 12:30:10 +0300

    fix: mongoose models fixed

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,75 +1,104 @@
 //const fs = require('fs');
 const Tour = require('./../models/tourModel');
 
-// const toursData = JSON.parse(
-//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, 'utf-8')
-// );
-
-// exports.chechID = (req, res, next, value) => {
-//   console.log(`Tour id is ${value}`);
-//   if (req.params.id * 1 > toursData.length) {
-//     return res.status(404).json({
-//       status: 'fail',
-//       message: 'Invalid ID'
-//     });
-//   }
-//   next();
-// };
-
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price)
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price'
+exports.getAllTours = async (req, res) => {
+  try {
+    const toursData = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      //requestedAt: req.requestTime,
+      results: toursData.length,
+      data: { tours: toursData }
     });
-  next();
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: 'success',
-    // requestedAt: req.requestTime,
-    // results: toursData.length,
-    // data: { tours: toursData }
-  });
+exports.getTour = async (req, res) => {
+  try {
+    // console.log(req.params); // data in the url are parameters
+    //const id = req.params.id * 1;
+    // Tour.findOne({ _id: req.params.id })
+    const tourData = await Tour.findById(req.params.id);
+    //const tourData = toursData[id];
+
+    res.status(200).json({
+      status: 'success',
+      data: { tour: tourData }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  console.log(req.params); // data in the url are parameters
-  const id = req.params.id * 1;
-  //const tourData = toursData[id];
+exports.createTour = async (req, res) => {
+  try {
+    //console.log(req.body); // body is avaible because of midleware
 
-  res.status(200).json({
-    status: 'success',
-    //data: { tour: tourData }
-  });
+    // const newTour = new Tour({
+    //   newTour.save()
+    // })
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    });
+    // rejected promises enter the catch block
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err //'Invalid data sent!'
+    });
+  }
 };
 
-exports.createTour = (req, res) => {
-  //console.log(req.body); // body is avaible because of midleware
-
-  res.status(201).json({
-    status: 'success',
-    // data: {
-    //   tour: newTour
-    // }
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const updatedTourData = await Tour.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true, // updated doc will be sent
+        runValidators: true
+      }
+    );
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: updatedTourData
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!'
+    });
+  }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here...>'
-    }
-  });
-};
-
-exports.deleteTour = (req, res) => {
-  // 204 means no content
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    // await for deletion
+    await Tour.findByIdAndDelete(req.params.id);
+    // 204 means no content
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!'
+    });
+  }
 };
--- models/tourModel.js
+++ models/tourModel.js
@@ -4,16 +4,55 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
-    unique: true
+    unique: true,
+    trim: true
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty']
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5
+  },
+  retingsQuantity: {
+    type: Number,
+    default: 0
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price']
-  }
+  },
+  priceDicount: {
+    type: Number
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  description: {
+    type: String,
+    trim: [true, 'A tour must have a description']
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have an image']
+  },
+  images: [String], // array of strings
+  createdAt: {
+    type: Date,
+    default: Date.now() // auto converted in mongoose
+  },
+  startDates: [Date] // unix timestamp
 });
 
 // model
